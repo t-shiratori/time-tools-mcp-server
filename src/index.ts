22d8449fa78609e5808c13fcf54fb799e150d71a
takashi <tks.srtr89@gmail.com>
2025-05-03 22:35:17 +0900

    update: indent

--- src/index.ts
+++ src/index.ts
@@ -7,61 +7,61 @@ const DEFAULT_TIME_FORMAT = "YYYY-MM-DD HH:mm:ss";
 
 // Create an MCP server
 const server = new McpServer({
-	name: "TimeTools",
-	description: "Tools to help you manage time",
-	version: "1.0.0",
+    name: "TimeTools",
+    description: "Tools to help you manage time",
+    version: "1.0.0",
 });
 
 server.tool(
-	"getCurrentDateTime",
-	"Get the current date and time (e.g. 2025-01-01 01:01:01)",
-	async () => {
-		const currentDateTime = dayjs().format(DEFAULT_TIME_FORMAT);
-		return {
-			content: [
-				{
-					type: "text",
-					text: currentDateTime,
-				},
-			],
-		};
-	},
+    "getCurrentDateTime",
+    "Get the current date and time (e.g. 2025-01-01 01:01:01)",
+    async () => {
+        const currentDateTime = dayjs().format(DEFAULT_TIME_FORMAT);
+        return {
+            content: [
+                {
+                    type: "text",
+                    text: currentDateTime,
+                },
+            ],
+        };
+    },
 );
 
 server.tool(
-	"getElapsedTime",
-	"Get the difference time between two datetimes (e.g. 2025-01-01 01:01:01 and 2025-01-02 02:02:02)",
-	{
-		from: z.string(),
-		to: z.string(),
-		unit: z.enum(["second", "minute", "hour"]).optional().default("second"),
-	},
-	async ({ from, to, unit = "second" }) => {
-		const fromDateTime = dayjs(from);
-		const toDateTime = dayjs(to);
-		if (!fromDateTime.isValid() || !toDateTime.isValid()) {
-			return {
-				content: [{ type: "text", text: "Invalid format" }],
-				isError: true,
-			};
-		}
-		/**
-		 * {@link https://day.js.org/docs/en/display/difference}
-		 */
-		const duration = toDateTime.diff(fromDateTime, unit, true);
-		return {
-			content: [{ type: "text", text: String(duration) }],
-		};
-	},
+    "getElapsedTime",
+    "Get the difference time between two datetimes (e.g. 2025-01-01 01:01:01 and 2025-01-02 02:02:02)",
+    {
+        from: z.string(),
+        to: z.string(),
+        unit: z.enum(["second", "minute", "hour"]).optional().default("second"),
+    },
+    async ({ from, to, unit = "second" }) => {
+        const fromDateTime = dayjs(from);
+        const toDateTime = dayjs(to);
+        if (!fromDateTime.isValid() || !toDateTime.isValid()) {
+            return {
+                content: [{ type: "text", text: "Invalid format" }],
+                isError: true,
+            };
+        }
+        /**
+         * {@link https://day.js.org/docs/en/display/difference}
+         */
+        const duration = toDateTime.diff(fromDateTime, unit, true);
+        return {
+            content: [{ type: "text", text: String(duration) }],
+        };
+    },
 );
 
 async function main() {
-	const transport = new StdioServerTransport();
-	await server.connect(transport);
-	console.error("Example MCP Server running on stdio");
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error("Example MCP Server running on stdio");
 }
 
 main().catch((error) => {
-	console.error("Fatal error in main():", error);
-	process.exit(1);
+    console.error("Fatal error in main():", error);
+    process.exit(1);
 });
